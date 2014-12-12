925402168567c0712214d957548e3e7a2e006b35
JPPinto <jose.vc.pinto@gmail.com>
2014-12-12 16:36:14 +0000

    HTML'ing

--- web_app/js/apiCalls.js
+++ web_app/js/apiCalls.js
@@ -1,6 +1,10 @@
+var baseURL = "http://192.168.34.200/Primavera/api/";
+var artigos = "Artigos";
+var clientes = "Clientes";
+
 $(document).ready(function() {
   // /api/Artigos
-  $.getJSON("http://192.168.56.101/Primavera/api/Artigos", function(data) {
+  $.getJSON(baseURL + artigos, function(data) {
       console.log("DATA " + JSON.stringify(data));
       for (var i = 0; i < data.length; i++) {
         //{"CodArtigo":"FC.0002","DescArtigo":"Cravos"}
@@ -12,11 +16,11 @@ $(document).ready(function() {
     });
 
   // /api/Clientes
-  $.getJSON("http://192.168.56.101/Primavera/api/Clientes", function(data) {
+  $.getJSON(baseURL + clientes, function(data) {
       console.log("DATA " + JSON.stringify(data));
       for (var i = 0; i < data.length; i++) {
         //{"CodCliente":"C0001","NomeCliente":"Restaurante ABC","NumContribuinte":"123456789","Moeda":"EUR","ModoPag":"NUM","CondPag":"1"}
-        if(data[i].CodCliente == "VD")
+        if (data[i].CodCliente == "VD")
           continue;
         $('table#tableClientes').append('<tr><td>' + data[i].CodCliente + '</td><td>' + data[i].NomeCliente + '</td><td>' + data[i].NumContribuinte + '</td></tr>');
       }
