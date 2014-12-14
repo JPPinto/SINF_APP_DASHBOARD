608eebe5c05086009723f4eb1be85ee2ac200e6a
Jose pinto <ei12164@linus20.(none)>
2014-12-14 23:54:42 +0000

    Edited facturacao.html; Added Bootstrap datepicker;

---
+++ web_app/js/facturacao.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+                
+                $('.input-daterange').datepicker({
+                  todayBtn:'true',
+                  viewMode: 'years',
+                  format: 'mm-yyyy',
+                  autoclose: 'true',
+                  minViewMode: "months"
+                });
+            
+            });
