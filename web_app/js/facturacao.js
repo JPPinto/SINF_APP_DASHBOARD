6fa02da3f799c235a6e7fe8ce61bedf1c9e41cb0
JPPinto <jose.vc.pinto@gmail.com>
2014-12-16 00:10:20 +0000

    Finished date picker with dropdown dynamically. And Pretty, very pretty =)

--- web_app/js/facturacao.js
+++ web_app/js/facturacao.js
@@ -1,11 +1,63 @@
-$(document).ready(function () {
-                
-                $('.input-daterange').datepicker({
-                  todayBtn:'true',
-                  viewMode: 'years',
-                  format: 'mm-yyyy',
-                  autoclose: 'true',
-                  minViewMode: "months"
-                });
-            
-            });
+$(document).ready(function(){
+
+});
+
+
+$('#submit').click(function(){
+  console.log("Start: " + $('#start').val());
+  console.log("End: " + $('#end').val());
+});
+
+$('#dia').click(function(){
+  $('#start').val('');
+  $('#end').val('');
+
+  $('#datepicker').datepicker("remove");
+  $('#datepicker').datepicker({
+    todayBtn:'true',
+    autoclose: 'true',
+    format: 'dd-mm-yyyy',
+    minViewMode: 'days'
+  });
+
+  $('.input-daterange').css('display','');
+  $('.dropdown-toggle').text('Dia');
+  $('.dropdown-toggle').append("<span class=\"caret\"></span>");
+});
+
+$('#mes').click(function(){
+  //$('.hero-unit').html($('#fake_from_container').html());
+  //$('.input-daterange', $('.hero-unit')).attr('id', 'datepickerMonth');
+
+  $('#start').val('');
+  $('#end').val('');
+
+  $('#datepicker').datepicker("remove");
+  $('#datepicker').datepicker({
+    todayBtn:'true',
+    autoclose: 'true',
+    format: 'mm-yyyy',
+    minViewMode: 'months'
+  });
+
+  $('.input-daterange').css('display','');
+  $('.dropdown-toggle').text('Mês');
+  $('.dropdown-toggle').append("<span class=\"caret\"></span>");
+});
+
+$('#ano').click(function(){
+  $('#start').val('');
+  $('#end').val('');
+
+  $('#datepicker').datepicker("remove");
+  $('#datepicker').datepicker({
+    todayBtn:'true',
+    autoclose: 'true',
+    format: 'yyyy',
+    minViewMode: 'years'
+  });
+
+  $('.input-daterange').css('display','');
+  $('.dropdown-toggle').text('Ano');
+  $('.dropdown-toggle').append("<span class=\"caret\"></span>");
+});
